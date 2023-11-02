5ec6235a3f3162bdd735a086fabd67dcca1e85d5
wirke <wiriyevich@gmail.com>
2023-11-02 16:48:21 +0100

    1.0

--- train.py
+++ train.py
@@ -1,11 +1,8 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("output.yaml")  # build a new model from scratch
 model = YOLO("yolov8n-pose.pt")  # load a pretrained model (recommended for training)
 
 # Use the model
-model.train(data="coco-pose.yaml", epochs=3)  # train the model
-metrics = model.val()  # evaluate model performance on the validation set
-results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
-path = model.export(format="onnx")  # export the model to ONNX format
+model.train(data="config.yaml", epochs=1)  # train the model
+
